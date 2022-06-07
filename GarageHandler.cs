6a58da9c25d9b53a42e6e45ccd378229713bb2c2
Laradomnin <lara.domnin@hotmail.com>
2022-06-07 17:33:03 +0200

    ui och count

--- GarageHandler.cs
+++ GarageHandler.cs
@@ -23,6 +23,7 @@ namespace Ovning_5___Garage_1._0
             Park(new Airplane("AIR222", "Boeing", "787", "White", 40));
             Park(new Airplane("AIR333", "Boeing", "787", "White", 40));
             Park(new Boat("BBB45", "Sandström", "Cabinbåt", "Blå", 6));
+            Park(new Bus("ABC142", "Scania", "Skolbuss", "Röd", 20));
         }
 
         public void Park(Vehicle vehicle)
@@ -62,6 +63,7 @@ namespace Ovning_5___Garage_1._0
         }
 
 
+        
         public void SearchByColorAndPrint(string color)
         {
           
@@ -72,6 +74,66 @@ namespace Ovning_5___Garage_1._0
                 Console.WriteLine(vehicle.ToString());
             }
         
+        }
+        public void SearchByType(string searchType)
+        {
+        // Car => Car
+
+        // car => Car
+        // bUS => Bus
+
+        // ca => Car
+        // TRUC => Truck
+        // b => boat, bus
+        
+           // var result = garage.Where(v => v.GetType().Name.ToLower() == searchType.ToLower());
+            var result = garage.Where(v => v.GetType().Name.ToLower().StartsWith(searchType));
+
+            foreach (var vehicle in result)
+            {
+                Console.WriteLine(vehicle.ToString());
+            }
+           
+        }
+
+        public void CountTypes()
+        {
+            // dumma sättet
+            int airplaneCount = 0;
+            int boatCount = 0;
+            int busCount = 0;
+            int carCount = 0;
+
+            foreach (var vehicle in garage)
+            {
+                if(vehicle.GetType().Name == "Car")
+                {
+                    carCount++;
+                }
+                if (vehicle.GetType().Name == "Boat")
+                {
+                    boatCount++;
+                }
+                if (vehicle.GetType().Name == "Bus")
+                {
+                    busCount++;
+                }
+                if (vehicle.GetType().Name == "Airplane")
+                {
+                    airplaneCount++;
+                }
+            }
+
+            Console.WriteLine($"Airplanes: {airplaneCount}");
+            Console.WriteLine($"Buses: {busCount}");
+            Console.WriteLine($"Boats: {boatCount}");
+            Console.WriteLine($"Car: {carCount}");
+            Console.WriteLine();
+
+            // smarta sättet
+            // Involverar Linq metoden GroupBy
+            var test = garage.GroupBy(v => v.GetType()).ToList();
+            
         }
     }
 }
---
+++ UI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovning_5___Garage_1._0
+{
+    internal class UI
+    {
+        internal static string AskForString()
+        {
+            Console.WriteLine(" Ange färg.");
+            return Console.ReadLine();
+        }
+    }
+}
